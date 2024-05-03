dc6adc24c811dff9391c6e939015e0b00033dc89
Bhanushree-avvari <bhanushreeavvari366@gmail.com>
2024-05-03 20:59:15 +0530

    protected.java

---
+++ protected.java
@@ -0,0 +1,22 @@
+package com.coreconcepts.accessmodifiers;
+
+public class ProtectedA {
+	
+	protected void display() {
+		System.out.println("TNS Sessions");
+	}
+
+}
+
+import com.coreconcepts.accessmodifiers.*;
+
+public class Protected2Main extends ProtectedA {
+	
+	public static void main(String[] args) {
+		
+		Protected2Main p3 = new Protected2Main();
+		p3.display();
+		
+	}
+
+}
