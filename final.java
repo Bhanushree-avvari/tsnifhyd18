cecb0fa0a108f0333f159ef1d614c7714851f5c8
Bhanushree-avvari <bhanushreeavvari366@gmail.com>
2024-05-27 22:34:14 +0530

    final.java

--- final.java
+++ final.java
@@ -10,28 +10,3 @@ public class FinalExtends extends FinalBike{
 		f1.run();
 		}
 }
-public class FinalMethod1 {
-		final void run() {
-		System.out.println("Running");
-	}
-}
-public class FinalMethodMain extends FinalMethod1{
-	void run() {
-		System.out.println("Running safely with 100kmph");
-	}
-public static void main(String[] args) {
-		FinalMethodMain f1 = new FinalMethodMain();
-		f1.run();
-	}
-}
-public class FinalVariable {
-	final int speedlimit=90; // constant
-	void run() {
-		speedlimit=125;
-	}
-public static void main(String[] args) 
-{
-		FinalVariable obj = new FinalVariable();
-		obj.run();
-}
-}
